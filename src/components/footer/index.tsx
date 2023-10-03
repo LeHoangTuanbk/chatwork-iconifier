531ec67eec5e7022d911912b3f4c6f8836bb21c7
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2023-10-03 22:41:19 +0900

    initial GUI

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small className="footer__copyright">
+        &copy; 2023 All rights reserved
+      </small>
+      <span>Developed by Tuan Le Hoang</span>
+    </footer>
+  );
+}
