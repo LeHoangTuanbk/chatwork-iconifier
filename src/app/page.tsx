5d44d3f43a543e25ebce827105296db9682482fd
tuan.lehoang@pricer.co.jp <43330556+LeHoangTuanbk@users.noreply.github.com>
2023-10-28 10:20:58 +0900

    Add regex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
-import SmilingFaceSunglasses from "@/../public/icons/smiling-face-sunglasses.gif";
-import Image from "next/image";
 import { FaRegCopy } from "react-icons/fa";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -10,53 +8,102 @@ const textareaMaxLength = 1000;
 const textareaRows = 10;
 
 export default function Homepage() {
-  const handleCopy = () => {
-    toast.success("Copied to clipboard!", {
-      duration: 2000,
-      position: "top-right",
-      icon: "👏",
-      style: {
-        background: "#1677ff",
-        color: "#fff",
-      },
-    });
+  const fakeResult = `お疲れ様です。
+これからのことを真剣に考えてくれてありがとうございます。(bow)
+  
+今からでも大丈夫です。
+D室も、会議室も使用中なので、廊下のテーブルでよろしいでしょうか？(think)`;
+
+  const [iconifiedOutput, setIconifiedOutput] = useState<any>(``);
+
+  const handleIconify = async () => {
+    // Call Open API here to classify text
+
+    // Set output
+    let output = replaceWithIcons(fakeResult);
+    console.log(output);
+    setIconifiedOutput(output);
+    // Show output to user
   };
+
+  const replaceWithIcons = (text: string) => {
+    // Define your replacement rules here
+    const replacements: Record<string, string> = {
+      "(bow)": `<span className="icon-bow" role="img" aria-label="Bowing Icon">
+          🙇‍♂️
+        </span>`,
+      "(think)": `<span className="icon-think" role="img" aria-label="Thinking Icon">
+          🤔
+        </span>`,
+    };
+
+    // Use regular expression to find and replace patterns
+    const pattern = /\(\w+\)/g;
+    const modifiedText = text.replace(
+      pattern,
+      (match) => replacements[match] || match
+    );
+
+    return modifiedText;
+  };
+
+  const handleCopyToClipBoard = async () => {
+    if ("clipboard" in navigator) {
+      try {
+        await navigator.clipboard.writeText(iconifiedOutput);
+        toast.success("Copied", {
+          duration: 2000,
+          position: "bottom-right",
+          icon: "👏",
+          style: {
+            background: "#1677ff",
+            color: "#fff",
+          },
+        });
+        return true;
+      } catch (err) {
+        console.error("Error copying text to clipboard:", err);
+        return false;
+      }
+    }
+  };
+
   return (
     <div>
       <div>
         <Toaster />
       </div>
       <div className="container">
-        <h1 className="container__title">
-          Iconify your chatwork messages{" "}
-          <Image
-            src={SmilingFaceSunglasses}
-            alt="Smiling face with sun glasses"
+        <div className="app">
+          <h1 className="app__title">Iconify your chatwork messages </h1>
+          <textarea
+            className="textarea"
+            placeholder="Paste your chatwork message here 🚀"
+            required={true}
+            maxLength={textareaMaxLength}
+            rows={textareaRows}
           />
-        </h1>
-        <textarea
-          className="container__textarea"
-          placeholder="Paste your chatwork message here 🚀"
-          required={true}
-          maxLength={textareaMaxLength}
-          rows={textareaRows}
-        />
-        <button className="container__button">Iconify ✨</button>
-        <span>Iconified result 💥</span>
-        <textarea
-          defaultValue={""}
-          className="container__textarea container__output"
-          readOnly={true}
-          placeholder="(Results will appear here 😊)"
-          maxLength={textareaMaxLength}
-          rows={textareaRows}
-        ></textarea>
-        <button
-          className="container__button container__button--copy"
-          onClick={handleCopy}
-        >
-          Copy <FaRegCopy />
-        </button>
+          <div className="">
+            <button className="button" onClick={handleIconify}>
+              Iconify ✨
+            </button>
+          </div>
+          <span>Iconified result 💥</span>
+          <textarea
+            value={iconifiedOutput}
+            className="textarea"
+            readOnly={true}
+            placeholder="(Results will appear here 😊)"
+            maxLength={textareaMaxLength}
+            rows={textareaRows}
+          ></textarea>
+          <button
+            className="button button--copy"
+            onClick={handleCopyToClipBoard}
+          >
+            Copy <FaRegCopy />
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/service/openai-api.ts
@@ -0,0 +1,1 @@
+//Call openai API here
